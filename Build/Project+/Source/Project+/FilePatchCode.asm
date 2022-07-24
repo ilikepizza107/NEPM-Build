d44fb60d7eded969444f886e9b140b3e5ee4e9f9
ilikepizza107 <twachtveitl107@gmail.com>
2022-07-24 10:16:20 -0500

    Edit Credits, Add Lethal League, Set groundwork for HD Textures, Add Music, Stage Cosmetics

--- Build/Project+/Source/Project+/FilePatchCode.asm
+++ Build/Project+/Source/Project+/FilePatchCode.asm
@@ -282,20 +282,9 @@ string "stageinfo/"                     @ $80550CA8 # This address might change 
 ###############################################################
 Set menus based on Code Menu Stagelist setting [Bird]
 ###############################################################
-* 20523400 00000000 # If 80523400 is equal to 1
-string "/menu2/mu_menumain.pac"         @ $806FB248
-string "mu_menumain_en.pac"             @ $817F62BC
-string "/menu2/sc_selcharacter2.pac"    @ $806FF308
-string "sc_selcharacter2_en.pac"        @ $817F634D
-string "/menu2/sc_selmap.pac"           @ $806FF3F0
-string "sc_selmap_en.pac"               @ $817F637C
-string "stageslot/"                   @ $80550C98 # This address might change and crash
-string "stageinfo/"                   @ $80550CA8 # This address might change and crash
-* E0000000 80008000
-
-* 24523400 00000001 # If 80523400 is greater to 1
-string "/menu2/mu_menumain.pac"         @ $806FB248
-string "mu_menumain_en.pac"             @ $817F62BC
+* 24523400 00000000 # If 80523400 is greater to 0
+string "/menu2/br_menumain.pac"         @ $806FB248
+string "br_menumain_en.pac"             @ $817F62BC
 string "/menu2/br_selcharacter2.pac"    @ $806FF308
 string "br_selcharacter2_en.pac"        @ $817F634D
 string "/menu2/br_selmap.pac"           @ $806FF3F0
