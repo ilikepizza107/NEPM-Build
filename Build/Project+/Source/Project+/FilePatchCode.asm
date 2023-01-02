73fa79ba3925856296ffd7b345f02032b5673d1e
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2023-01-02 15:38:34 +0300

    Fix stagelist code to allow for new Replays.asm to be used

--- Build/Project+/Source/Project+/FilePatchCode.asm
+++ Build/Project+/Source/Project+/FilePatchCode.asm
@@ -275,8 +275,8 @@ string "/menu2/sc_selcharacter2.pac"    @ $806FF308
 string "sc_selcharacter2_en.pac"        @ $817F634D
 string "/menu2/sc_selmap.pac"           @ $806FF3F0
 string "sc_selmap_en.pac"               @ $817F637C
-string "stageslot/"                     @ $80550C98 # This address might change and crash
-string "stageinfo/"                     @ $80550CA8 # This address might change and crash
+string "stageslot/"                     @ $80550C58 # This address might change and crash
+string "stageinfo/"                     @ $80550C68 # This address might change and crash
 * E0000000 80008000
 
 ###############################################################
@@ -289,8 +289,8 @@ string "/menu2/br_selcharacter2.pac"    @ $806FF308
 string "br_selcharacter2_en.pac"        @ $817F634D
 string "/menu2/br_selmap.pac"           @ $806FF3F0
 string "br_selmap_en.pac"               @ $817F637C
-string "stageslotbr/"                   @ $80550C98 # This address might change and crash
-string "stageinfobr/"                   @ $80550CA8 # This address might change and crash
+string "stageslotbr/"                   @ $80550C58 # This address might change and crash
+string "stageinfobr/"                   @ $80550C68 # This address might change and crash
 * E0000000 80008000
 
 ##############################################################################################################################
