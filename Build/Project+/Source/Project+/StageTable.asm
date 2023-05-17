e48ecb324f19c35aa652bd5c238c181af026a55f
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2023-05-17 13:17:11 -0500

    Prep for 2.4

--- Build/Project+/Source/Project+/StageTable.asm
+++
@@ -1,6 +0,0 @@
-Stage Select Screen Table Data
-
-.include Source/Stagelists/ILPMStagelist.asm
-.include Source/Stagelists/InvincibleStagelist.asm
-.include Source/Stagelists/Middle3Stagelist.asm
-.include Source/Stagelists/PMBRStagelist.asm
