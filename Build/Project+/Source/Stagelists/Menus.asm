4804f818a324ec179a021904998733c68070416f
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2023-05-19 10:20:32 -0500

    Stagelist switcher
    Everything works except switching stageslot folders, leaving out Menus.asm for now

---
+++ Build/Project+/Source/Stagelists/Menus.asm
@@ -0,0 +1,41 @@
+###############################################################
+Set menus based on Code Menu Stagelist setting [Bird]
+###############################################################
+* 20523400 00000000 # If 80523400 is equal to 0
+string "/menu2/mu_menumain.pac"         @ $806FB248
+string "mu_menumain_en.pac"             @ $817F62BC
+string "/menu2/sc_selcharacter2.pac"    @ $806FF308
+string "sc_selcharacter2_en.pac"        @ $817F634D
+string "/menu2/sc_selmap.pac"           @ $806FF3F0
+string "sc_selmap_en.pac"               @ $817F637C
+string "stageslot/"                     @ $80550C58 # This address might change and crash
+string "stageinfo/"                     @ $80550C68 # This address might change and crash
+* E0000000 80008000
+
+###############################################################
+Set menus based on Code Menu Stagelist setting [Bird]
+###############################################################
+* 20523400 00000001 # If 80523400 is equal to 1
+string "/menu2/mu_menumain.pac"         @ $806FB248
+string "mu_menumain_en.pac"             @ $817F62BC
+string "/menu2/sc_selcharacter2.pac"    @ $806FF308
+string "sc_selcharacter2_en.pac"        @ $817F634D
+string "/menu2/sc_selmap.pac"           @ $806FF3F0
+string "sc_selmap_en.pac"               @ $817F637C
+string "stageslot/"                     @ $80550C58 # This address might change and crash
+string "stageinfo/"                     @ $80550C68 # This address might change and crash
+* E0000000 80008000
+
+###############################################################
+Set menus based on Code Menu Stagelist setting [Bird]
+###############################################################
+* 24523400 00000001 # If 80523400 is greater to 1
+string "/menu2/mu_menumain.pac"         @ $806FB248
+string "mu_menumain_en.pac"             @ $817F62BC
+string "/menu2/sc_selcharacter2.pac"    @ $806FF308
+string "sc_selcharacter2_en.pac"        @ $817F634D
+string "/menu2/sc_selmap.pac"           @ $806FF3F0
+string "sc_selmap_en.pac"               @ $817F637C
+string "stageslotbr/"                   @ $80550C58 # This address might change and crash
+string "stageinfo/"                     @ $80550C68 # This address might change and crash
+* E0000000 80008000
