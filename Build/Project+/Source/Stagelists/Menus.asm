67083abd66115aa44528ab18248f99967f42915b
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2023-05-22 12:31:16 -0500

    Cosmetics and br menus

--- Build/Project+/Source/Stagelists/Menus.asm
+++ Build/Project+/Source/Stagelists/Menus.asm
@@ -30,12 +30,12 @@ string "stageinfo/"                     @ $80550B18 # This address might change 
 Set menus based on Code Menu Stagelist setting [Bird]
 ###############################################################
 * 24523400 00000001 # If 80523400 is greater to 1
-string "/menu2/mu_menumain.pac"         @ $806FB248
-string "mu_menumain_en.pac"             @ $817F62BC
-string "/menu2/sc_selcharacter2.pac"    @ $806FF308
-string "sc_selcharacter2_en.pac"        @ $817F634D
-string "/menu2/sc_selmap.pac"           @ $806FF3F0
-string "sc_selmap_en.pac"               @ $817F637C
+string "/menu2/br_menumain.pac"         @ $806FB248
+string "br_menumain_en.pac"             @ $817F62BC
+string "/menu2/br_selcharacter2.pac"    @ $806FF308
+string "br_selcharacter2_en.pac"        @ $817F634D
+string "/menu2/br_selmap.pac"           @ $806FF3F0
+string "br_selmap_en.pac"               @ $817F637C
 string "stageslotbr/"                   @ $80550B08 # This address might change and crash
 string "stageinfo/"                     @ $80550B18 # This address might change and crash
 * E0000000 80008000
