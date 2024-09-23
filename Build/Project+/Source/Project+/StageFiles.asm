b556bef659514b68353d1544879c7d0a7c3f2acf
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2024-09-23 14:17:29 -0500

    Update to P+ 3.0.5
    Also changed Kulprit's MK previews + added new tags

--- Build/Project+/Source/Project+/StageFiles.asm
+++ Build/Project+/Source/Project+/StageFiles.asm
@@ -127,6 +127,8 @@ HOOK @ $80949BEC
 	lhz r4, -0xFEE(r12)		# / Effectbank ID to load @ $8054F012
 	stw r4, 0x94(r3)   	 	# store stage effectbank ID, not read by SSE
 }
+/*
+### NO LONGER USED, LEFT HERE FOR NOW TO REMEMBER HOW IT WAS ACHIEVED
 ###
 # Load RGBA Overlay
 # Suggested values for overlays:
@@ -154,7 +156,7 @@ HOOK @ $8083413c	# Fighter::postInitialize
 	mtctr r12 				# |
 	bctr					# /
 }
-
+*/
 ###
 # Load Secondary Stage Name on Slots 0x13 and 0x19
 op NOP @ $8094AB24			# Make all dual pac stages run the same code
